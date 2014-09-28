1daec2202990d5cb2f7f4f00711adc034951ea5a
Ubikcircle <ubikcircle@ubik.com>
2014-09-28 10:03:30 +0300

    Добавил полковника Лебеденко. Изменил утилиту рендеринга.

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ name=${1:-links}
 
 for t in svg png
 do
-	php -f generateXML.php | xsltproc generateDOT.xsl - | neato -T$t -o$name.$t
+	php -f generateXML.php | xsltproc generateDOT.xsl - | fdp -T$t -o$name.$t
 done
